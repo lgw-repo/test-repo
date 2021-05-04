b64b280d465fd50054082b5e44f560a39ecfe025
lgw <2662265164@qq.com>
2021-05-04 15:09:55 +0800

    添加项目到仓库

---
+++ test/src/entity/Person.java
@@ -0,0 +1,19 @@
+package entity;
+
+public class Person {
+	public int age;
+	public String name;
+
+	public Person(int age, String name) {
+		this.age = age;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person{" +
+				"age=" + age +
+				", name='" + name + '\'' +
+				'}';
+	}
+}
---
+++ test/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+import entity.Person;
+
+
+public class Test {
+    public static void main(String[] args) {
+        Person p = new Person(10, "ะกร๗");
+        System.out.println(p);
+    }
+}
